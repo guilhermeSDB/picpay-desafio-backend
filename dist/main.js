0a71f826d1cae475f947b520784cda0c54700f25
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-11-27 16:36:42 -0300

    initial commit with fastify and swc

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _core = require("@nestjs/core");
+const _platformfastify = require("@nestjs/platform-fastify");
+const _appmodule = require("./app.module");
+async function bootstrap() {
+    const app = await _core.NestFactory.create(_appmodule.AppModule, new _platformfastify.FastifyAdapter());
+    await app.listen(3000);
+}
+bootstrap();
