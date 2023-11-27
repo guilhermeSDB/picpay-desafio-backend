dc413b93ac31c88e4b9c7c3217581fdace9487ad
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-11-27 16:50:33 -0300

    feat: upgrade gitignore and editorconfig

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+
 import {
 	FastifyAdapter,
 	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(3000);
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+
+	const config = new DocumentBuilder()
+
+		.setTitle('Picpay API')
+
+		.setDescription('The Picpay API documentation')
+
+		.setVersion('1.0')
+
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(3000);
 }
+
 bootstrap();
