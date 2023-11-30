92171ac48fbde5809dba5eb0add080e1c6e1d8ba
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-11-30 09:09:42 -0300

    refactor: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ModelsModule } from './models/models.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [SharedModule, ModelsModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
