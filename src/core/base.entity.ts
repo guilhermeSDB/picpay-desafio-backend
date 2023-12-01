3e0b210cd8f15a5699fc18c8cf51bd76944b0da5
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-12-01 00:47:57 -0300

    feat: create base entity

---
+++ src/core/base.entity.ts
@@ -0,0 +1,23 @@
+import {
+	CreateDateColumn,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@CreateDateColumn({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP(6)',
+	})
+	public createdAt: Date;
+
+	@UpdateDateColumn({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP(6)',
+		onUpdate: 'CURRENT_TIMESTAMP(6)',
+	})
+	public updatedAt: Date;
+}
