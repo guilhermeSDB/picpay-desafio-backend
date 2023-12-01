36bee93cf3a3a3e3bf17b332efb8b5ddbd5f6cc3
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-12-01 00:48:55 -0300

    feat: update models module

--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { NotificationModule } from './notification/notification.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
 
-const Models = [TransactionModule, UserModule];
+const Models = [TransactionModule, UserModule, NotificationModule];
 
 @Module({
 	imports: [...Models],
